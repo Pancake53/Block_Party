6571600e2c4ba2932f0779f12cd0886942c0ed03
Nikolai Sinenko <nisine@utu.fi>
2026-01-28 20:58:15 +0200

    Character class collision and update pos function
    Closes #4

--- GameObject.py
+++ GameObject.py
@@ -7,14 +7,17 @@ class GameObject():
         self.x_speed = 0
         self.y_speed = 0
         self.gravity = 0.5
+        self.air_resistance = 0.99
+        self.selected = False
         self.rect = pygame.Rect(x_pos, y_pos, 16, 32)
 
     def render(self, surface):
-        pygame.draw.rect(surface, self.colour, self.charRect)
+        pygame.draw.rect(surface, self.colour, self.rect)
 
     def update(self, dt, actions, tiles):
-        self.update_pos()
-        self.move(tiles)
+        
+        if (self.x_speed > 0) or (self.y_speed > 0):
+            self.update_pos(tiles)
 
     def collision_test(self, tiles):
         collisions = []
@@ -23,11 +26,16 @@ class GameObject():
                 collisions.append(tile)
         return collisions
     
-    def move(self, tiles):
+    def update_pos(self, tiles):
+        # self.y_pos += self.y_speed\
+        # self.rect.y = self.y_pos
+        # collision_test
+        # self.x_pos += self.x_speed
+        # self.rect.x = self.x_pos
+        # collision_test
         pass
 
-    def update_pos(self):
-        self.y_pos += self.y_speed
-        self.x_pos += self.x_speed
-        self.rect.x = self.x_pos
-        self.rect.y = self.y_pos
+    def add_momentum(self, x_speed, y_speed):
+        self.x_speed = x_speed
+        self.y_speed = y_speed
+
--- character.py
+++ character.py
@@ -8,28 +8,60 @@ class Character(GameObject):
         self.team_id = team_id
         self.colour = [(184, 43, 43), (95, 184, 43)][team_id]
         self.width = width
-        self.bounce_stop = 0.4
-        self.retention = 0.8 / width
+        self.y_stop = 1
+        self.x_stop = 1
+        self.retention = 0.25 # * width
+
         self.rect = pygame.Rect(x_pos, y_pos, 16 * width, 32)
-
-    def render(self, surface):
-        pygame.draw.rect(surface, self.colour, self.rect)
-
 
     def collision_test(self, tiles):
         collisions = []
         for tile in tiles:
-            if self.rect.colliderect(tile):
+            if self.rect.colliderect(tile): # returns a boolean
                 collisions.append(tile)
         return collisions
     
-    def move(self, tiles):
+    def update_pos(self, tiles): # only active if we have speed
+        self.update_pos_x(tiles)
+        self.update_pos_y(tiles)
+
+    def update_pos_x(self, tiles):
+        # x movement
+        self.x_pos += self.x_speed # for calculations
+        self.rect.x = self.x_pos # update rect position for collision_test
         collisions = self.collision_test(tiles)
+
+        if not collisions:
+            if self.x_speed < self.x_stop:
+                self.x_speed = 0
+            else:
+                self.x_speed *= self.air_resistance
+        else: # collision on x axis
+            tile = collisions[0]
+            self.x_speed = - self.x_speed * self.retention
+
+            if self.x_speed > 0:
+                self.rect.right = tile.left # colliderect => False
+            elif self.x_speed < 0:
+                self.rect.left = tile.right # colliderect => False
+
+    def update_pos_y(self, tiles):
+                # y movement
+        self.y_pos += self.y_speed # for calculations
+        self.rect.y = self.y_pos # update rect w calculated pos
+        collisions = self.collision_test(tiles)
+
         if not collisions:
             self.y_speed += self.gravity
-        elif self.y_speed > self.bounce_stop:
-            self.y_speed = self.y_speed * -1 * self.retention
-        else:
-            self.y_speed = 0
-    
+        else: # collision on y axis
+            tile = collisions[0]
+
+            if self.y_speed > self.y_stop: # top
+                self.rect.bottom = tile.top
+                self.y_speed = self.y_speed * -1 * self.retention
+            elif -self.y_speed > self.y_stop: # bottom
+                self.rect.top = tile.bottom
+                self.y_speed = self.y_speed * -1 * self.retention / 2
+            else:
+                self.y_speed = 0
     
