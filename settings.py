0c7569d1f31ebdb4b8bdb5fa81ff5eabe94116ad
Nikolai Sinenko <nisine@utu.fi>
2026-04-06 22:56:30 +0300

    settings class and game music file picked

---
+++ settings.py
@@ -0,0 +1,21 @@
+
+
+class Settings():
+    def __init__(self):
+
+        # Audio
+        self.master_volume = 100
+        self.music_vol = 100
+        self.sfx_vol = 100
+        self.dialogue_vol = 100
+
+        self.mute = False
+        self.skip_dialogue = False
+
+        # screen
+        self.FULLSCREEN = False
+        self.SCREEN_W = 960
+        self.SCREEN_H = 540
+
+        # 
+        
